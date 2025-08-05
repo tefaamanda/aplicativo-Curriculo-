5f00c0d2bc94eb971f09228cfc9cfca76e2b5437
Sthefany Amanda <64119673+tefaamanda@users.noreply.github.com>
2025-08-05 14:42:46 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -5,13 +5,44 @@ export default function App() {
   return (
     <ScrollView>
       <Image source={require('./assets/Image.png')} style={styles.imagem}></Image>
-      <Text style={styles.texto1}>Sthefany Amanda Martins de Moura</Text>
-      <View>
-        <Text>DADOS PESSOAIS</Text>
-        <Text>Endereço: Bairro Perdizes, Birigui-SP</Text>
-        <Text>CEP: 16200-137</Text>
-        <Text>Idade: 17 anos</Text>
-        <Text>Nacionalidade: Brasileira</Text>
+      <Text style={styles.nome}>Sthefany Amanda Martins de Moura</Text>
+      <View style={styles.bloco}>
+        <Text style={styles.titulo}>DADOS PESSOAIS</Text>
+        <Text style={styles.texto}>Endereço: Bairro Perdizes, Birigui-SP</Text>
+        <Text style={styles.texto}>CEP: 16200-137</Text>
+        <Text style={styles.texto}>Idade: 17 anos</Text>
+        <Text style={styles.texto}>Nacionalidade: Brasileira</Text>
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.titulo}>FORMAÇÃO ACADÊMICA</Text>
+        <Text style={styles.subtitulo}>Curso Técnico em Desenvolvimento de Sistemas (T.I.) - Escola SENAI "Avak Bedouian"</Text>
+        <Text style={styles.texto}>Início: janeiro de 2024 | Previsão de conclusão: dezembro de 2025</Text>
+        <Text style={styles.subtitulo}>Escola SESI "Samir Nakad" - Cursando o segundo ano do Ensino Médio</Text>
+        <Text style={styles.texto}>Início: janeiro de 2018 | Previsão de conclusão: dezembro de 2025</Text>
+        <Text style={styles.subtitulo}>Escola Municipal de Ensino Fundamental Doutor Gama</Text>
+        <Text style={styles.texto}>Início: fevereiro de 2012 | Conclusão: dezembro de 2017</Text>
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.titulo}>EXPERIÊNCIAS PROFISSIONAIS</Text>
+        <Text style={styles.subtitulo}>Pequito Calçados</Text>
+        <Text style={styles.texto}>Período: Atualmente</Text>
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.titulo}>HABILIDADES INTERPESSOAIS</Text>
+        <Text style={styles.texto}>Capacidade de trabalho em equipe.</Text>
+        <Text style={styles.texto}>Espírito de liderança.</Text>
+        <Text style={styles.texto}>Proatividade.</Text>
+        <Text style={styles.texto}>Resiliência.</Text>
+        <Text style={styles.texto}>Criatividade.</Text>
+        <Text style={styles.texto}>Empatia.</Text>
+        <Text style={styles.texto}>Capacidade de resolução de conflitos.</Text>
+        <Text style={styles.texto}>Comprometimento.</Text>
+        <Text style={styles.texto}>Pensamento crítico.</Text>
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.titulo}>IDIOMAS</Text>
+        <Text style={styles.texto}>Português fluente (Nativo).</Text>
+        <Text style={styles.texto}>Inglês Intermediário.</Text>
       </View>
       <StatusBar style="auto" />
     </ScrollView>
@@ -19,31 +50,58 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    gap: 30
+
+
+  nome: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontFamily: 'Arial',
+    paddingTop: 15,
+    paddingBottom: 15,
   },
 
-  texto1: {
+  bloco: {
+    backgroundColor: '#f9f9f9',
+    padding: 15,
+    margin: 10,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+
+  titulo: {
       color: 'black',
       fontSize: 20,
-    textAlign: 'center',
-    paddingTop: 20,
+      textAlign: 'center',
+      fontWeight: 'bold',
   },
+
+  subtitulo: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  texto: {
+    color: 'black',
+    fontSize: 15,
+  },
+
   imagem: {
     marginTop: 20,
-    width: '200',
-    height: '200',
+    width: 250,
+    height: 250,
+    borderRadius: 140,
     resizeMode: 'cover',
     alignSelf: 'center',
+    borderWidth: 2,
+    borderColor: '#000',
   },
-  quadradoPai: {
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
-    flex: 1,
-    margin: 40,
-  }
+
 });
 
